a4d1b217f5ffb3a3de218bf32852f6c75f68056b
oceanfu0000 <ocean_fudahai@hotmail.com>
2025-03-16 00:59:45 +0800

    entering park fix

--- enteringpark/enteringpark.py
+++ enteringpark/enteringpark.py
@@ -8,7 +8,6 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 
 import requests
-from invokes import invoke_http
 
 #region Create a Blueprint for enter_park routes
 app = Flask(__name__)
@@ -64,3 +63,48 @@ app.register_blueprint(enter_park_blueprint, url_prefix="/enter_park")
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8085, debug=True)
 #endregion
+
+# region Invoking Methods
+SUPPORTED_HTTP_METHODS = set([
+    "GET", "OPTIONS", "HEAD", "POST", "PUT", "PATCH", "DELETE"
+])
+
+def invoke_http(url, method='GET', json=None, **kwargs):
+    """A simple wrapper for requests methods.
+       url: the url of the http service;
+       method: the http method;
+       json: the JSON input when needed by the http method;
+       return: the JSON reply content from the http service if the call succeeds;
+            otherwise, return a JSON object with a "code" name-value pair.
+    """
+    code = 200
+    result = {}
+
+    try:
+        if method.upper() in SUPPORTED_HTTP_METHODS:
+            r = requests.request(method, url, json=json, **kwargs)
+        else:
+            raise Exception(f"HTTP method {method} unsupported.")
+    except Exception as e:
+        code = 500
+        result = {"code": code, "message": f"Invocation of service fails: {url}. {str(e)}"}
+
+    if code not in range(200, 300):  # If not a success status code
+        result['code'] = code
+        return result
+
+    # Check http call result
+    if r.status_code != requests.codes.ok:
+        code = r.status_code
+        result['code'] = code
+
+    # Try to parse JSON result
+    try:
+        result = r.json() if len(r.content) > 0 else {"code": code, "message": "Empty response body"}
+    except Exception as e:
+        code = 500
+        result = {"code": code, "message": f"Invalid JSON output from service: {url}. {str(e)}"}
+
+    result['code'] = code  # Ensure the status code is added to the result
+    return result
+# endregion
